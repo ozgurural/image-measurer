d0a5b1fa6fbfa83d4f4655d2da0dca264149a28a
Ozgur Ural <ozzgural@gmail.com>
2023-07-16 11:16:12 +0300

    v2

--- image_measurer.py
+++ image_measurer.py
@@ -1,110 +1,190 @@
-import cv2
+import sys
+from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QMainWindow, QFileDialog, QAction, QInputDialog, QStatusBar, QToolBar, QMessageBox, QTextBrowser
+from PyQt5.QtGui import QPixmap, QPainter, QPen, QFont, QIcon, QImage
+from PyQt5.QtCore import QPointF, Qt, QRectF
 import numpy as np
-import tkinter as tk
-from tkinter import simpledialog
-import PySimpleGUI as sg
-import os
 
-# global variables
-ix, iy = -1, -1
-drawing = False
-point1, point2 = None, None
-reference = None
-real_world_distance = None
-fullscreen = False
 
-# function to open a dialog and ask for real world distance
-def ask_for_distance():
-    root = tk.Tk()
-    root.withdraw()
-    distance = simpledialog.askfloat("Input", "Please enter the real world distance for the reference:", minvalue=0.0)
-    return distance
+class ImageWithMouseControl(QGraphicsView):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.resize(800, 600)  # set initial size to 800x600
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+        self.points = []
+        self.pointPen = QPen(Qt.red, 10)
+        self.linePen = QPen(Qt.green, 2)
+        self.textPen = QPen(Qt.white, 2)
+        self.reference = None
+        self.real_world_distance = None
+        self.imageItem = None
+        self.lastPos = None
+        self.currentPos = None
+        self.currentLine = None
 
-# mouse callback function
-def draw_circle(event, x, y, flags, param):
-    global ix, iy, drawing, point1, point2, reference, real_world_distance
+    def reset(self):
+        self.points.clear()
+        self.reference = None
+        self.real_world_distance = None
+        self.scene.clear()
+        self.imageItem = None
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix, iy = x, y
-        if point1 is None:
-            point1 = [ix, iy]
-            cv2.circle(img, (ix, iy), 3, (0, 0, 255), -1)  # red point for point1
-        elif point2 is None:
-            point2 = [ix, iy]
-            cv2.circle(img, (ix, iy), 3, (0, 0, 255), -1)  # red point for point2
+    def setImage(self, image_path):
+        self.reset()
+        pixmap = QPixmap(image_path)
+        self.imageItem = self.scene.addPixmap(pixmap)
+        self.setSceneRect(QRectF(pixmap.rect()))
 
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            pass
+    def mousePressEvent(self, event):
+        if self.imageItem is None:
+            return super().mousePressEvent(event)
 
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        if point1 is not None and point2 is not None:
-            cv2.line(img, tuple(point1), tuple(point2), (0, 255, 0), 2)  # green line
-            pixel_distance = np.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+        if event.button() == Qt.LeftButton:
+            pos = self.mapToScene(event.pos())
+            if self.imageItem.contains(pos):
+                self.points.append(pos)
+                if len(self.points) == 2:
+                    self.drawMeasurement()
 
-            if reference is None:  # if it is the first measurement
-                reference = pixel_distance  # set the reference pixel distance
-                real_world_distance = ask_for_distance()  # open a dialog to ask for distance
-            else:  # calculate real world distance for the next measurements
-                distance = round((pixel_distance / reference) * real_world_distance, 4)
-                cv2.putText(img, str(distance), (point2[0], point2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)  # white text
-            
-            point1, point2 = None, None  # reset points
+    def drawMeasurement(self):
+        point1, point2 = self.points[:2]
+        line = self.scene.addLine(point1.x(), point1.y(), point2.x(), point2.y(), self.linePen)
+        distance = np.sqrt((point1.x() - point2.x())**2 + (point1.y() - point2.y())**2)
 
-# PySimpleGUI window for file selection
-layout = [  # layout for the GUI
-    [sg.Text("Choose an image file to measure: ")],
-    [sg.Input(), sg.FileBrowse(file_types=(("All Files", "*.*"),), key="-IN-"), sg.Button("Open")]
-]
-
-window = sg.Window("Image Selection", layout)
-
-while True:
-    event, values = window.read()
-    if event == "Open":
-        file_path = values["-IN-"]
-        _, file_extension = os.path.splitext(file_path)
-        if file_extension.lower() in ['.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff']:
-            break
+        if self.reference is None:
+            self.reference = distance
+            self.real_world_distance, ok = QInputDialog.getDouble(self, 'Real World Distance', 'Enter the real world distance for the reference:')
         else:
-            sg.Popup('Error', 'Please select an image file (jpg, jpeg, png, bmp, tif, tiff)')
-    elif event == sg.WINDOW_CLOSED:
-        break
+            real_distance = round((distance / self.reference) * self.real_world_distance, 4)
+            text = self.scene.addText(str(real_distance), QFont('Arial', 10))
+            text.setPos(point2)
+            text.setDefaultTextColor(Qt.white)
+        self.points.clear()
 
-window.close()
+    def wheelEvent(self, event):
+        factor = 1.25 if event.angleDelta().y() > 0 else 0.8
+        self.scale(factor, factor)
 
-# loading the image
-if 'file_path' in globals():
-    img = cv2.imread(file_path)
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback('image', draw_circle)
+    def mouseMoveEvent(self, event):
+        if self.imageItem is None:
+            return super().mouseMoveEvent(event)
 
-    while True:
-        try:
-            # this will throw an error if the window is closed
-            prop = cv2.getWindowProperty('image', 0)
-            
-            # display the image in the window
-            cv2.imshow('image', img)
+        self.currentPos = self.mapToScene(event.pos())
 
-            # if the window is open, then prop should be >= 0
-            if prop < 0:
-                break
+        if event.buttons() == Qt.RightButton:
+            if self.lastPos is not None:
+                diff = event.pos() - self.lastPos
+                self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() - diff.x())
+                self.verticalScrollBar().setValue(self.verticalScrollBar().value() - diff.y())
+            self.lastPos = event.pos()
+        elif event.buttons() == Qt.LeftButton:
+            self.drawCurrentLine()
 
-            key = cv2.waitKey(1)
-            if key & 0xFF == ord('f'):  # 'f' for fullscreen
-                fullscreen = not fullscreen
-                if fullscreen:
-                    cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-                else:
-                    cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
-            elif key & 0xFF == 27:  # escape key
-                break
-        except cv2.error:
-            break
+    def mouseReleaseEvent(self, event):
+        if self.imageItem is None:
+            return super().mouseReleaseEvent(event)
 
-    cv2.destroyAllWindows()
-else:
-    print("No file was selected.")
+        if event.button() == Qt.RightButton:
+            self.lastPos = None
+        elif event.button() == Qt.LeftButton:
+            if self.currentLine is not None and self.currentLine.scene() is not None:
+                self.scene.removeItem(self.currentLine)
+            self.currentLine = None
+
+    def drawCurrentLine(self):
+        if len(self.points) == 1 and self.currentPos:
+            if self.currentLine is not None and self.currentLine.scene() is not None:
+                self.scene.removeItem(self.currentLine)
+            self.currentLine = self.scene.addLine(self.points[0].x(), self.points[0].y(), self.currentPos.x(), self.currentPos.y(), self.linePen)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.imageViewer = ImageWithMouseControl(self)
+        self.setCentralWidget(self.imageViewer)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        openAction = QAction(QIcon('open_icon.png'), 'Open', self)
+        openAction.setShortcut('Ctrl+O')
+        openAction.triggered.connect(self.openImage)
+        openAction.setStatusTip('Open Image')
+        fileMenu.addAction(openAction)
+
+        saveAction = QAction(QIcon('save_icon.png'), 'Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.triggered.connect(self.saveImage)
+        saveAction.setStatusTip('Save Image')
+        fileMenu.addAction(saveAction)
+
+        exitAction = QAction(QIcon('exit_icon.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(QApplication.instance().quit)
+        exitAction.setStatusTip('Exit Application')
+        fileMenu.addAction(exitAction)
+
+        toolbar = QToolBar('Main Toolbar')
+        toolbar.addAction(openAction)
+        toolbar.addAction(saveAction)
+        toolbar.addAction(exitAction)
+        self.addToolBar(toolbar)
+
+        self.statusbar = self.statusBar()
+
+        # Add 'About' menu
+        helpMenu = self.menuBar().addMenu('Help')
+        aboutAction = QAction(QIcon('about_icon.png'), 'About', self)
+        aboutAction.triggered.connect(self.showAboutInfo)
+        helpMenu.addAction(aboutAction)
+
+        # Add this attribute
+        self.aboutBrowser = QTextBrowser()
+        self.aboutBrowser.setWindowTitle("About Image Measurer")
+        self.aboutBrowser.setText(
+            "This application is created by Ozgur Ural.\n\n"
+            "For more information, you can visit:"
+            "<a href='https://ozgurural.github.io/'>https://ozgurural.github.io/</a>")
+        self.aboutBrowser.setOpenExternalLinks(True)
+        self.aboutBrowser.resize(400, 200)  # You can adjust the size here
+
+    def showAboutInfo(self):
+        self.aboutBrowser.show()
+
+    def openImage(self):
+        image_path, _ = QFileDialog.getOpenFileName(self, 'Open file', '', "Image files (*.jpg *.jpeg *.png *.bmp *.tif *.tiff)")
+        if image_path:
+            self.imageViewer.setImage(image_path)
+            self.statusbar.showMessage('Image Loaded')
+        else:
+            QMessageBox.warning(self, 'Error', 'Could not open image')
+        
+    def saveImage(self):
+        # Create a QImage object with the same dimensions as the scene
+        img = QImage(self.imageViewer.scene.sceneRect().size().toSize(), QImage.Format_ARGB32)
+        
+        # Create a QPainter object and render the scene into the QImage object
+        painter = QPainter(img)
+        self.imageViewer.scene.render(painter)
+        painter.end()
+
+        # Open a QFileDialog in save file mode and get the selected file path
+        saved_path, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', 'PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*)')
+
+        if saved_path:
+            # Save the QImage object to the selected file
+            img.save(saved_path)
+            self.statusbar.showMessage('Image Saved')
+        else:
+            QMessageBox.warning(self, 'Error', 'Could not save image')
+
+
+def main():
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
